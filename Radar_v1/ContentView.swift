b553cc351605ed9d4b8eeafc203f3968fd93963e
Việt Anh Đoàn <vietanhd@dol-tech.com>
2019-11-06 10:52:54 +0700

    commit

--- Radar_v1/ContentView.swift
+++ Radar_v1/ContentView.swift
@@ -9,9 +9,44 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+    
+    @State var isSpinning = false
+    @State public var first = true
+    
+    struct Radar: Identifiable {
+        var id = UUID().uuidString
+        var name: String
+        var degrees: Double
     }
+    
+    let radars = [Radar(name: "outer", degrees: -180), Radar(name: "inner", degrees: 180)]
+    
+    var backgroundView: some View {
+        RadialGradient(gradient: Gradient(colors: [Color.white, .blue]), center: .center, startRadius: 5, endRadius: 500)
+        .scaleEffect(1.2)
+    }
+    
+    var radarView: some View {
+        ForEach(radars) {
+            Image($0.name)
+                .rotationEffect(.degrees(self.isSpinning ? $0.degrees : 0))
+                .animation(Animation.easeOut(duration: 1).repeatForever(autoreverses: true))
+                .onAppear {
+                    if self.first {
+                        self.first = false
+                        self.isSpinning.toggle()
+                    }
+            }
+        }
+    }
+    
+    var body: some View {
+        ZStack {
+            backgroundView
+            radarView
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
